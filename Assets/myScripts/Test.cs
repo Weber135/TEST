22b5f1e4ebdc1fde768f0c479630a607bbc14b2a
Conor Gorman <iamgoreman15@gmail.com>
2020-11-11 14:29:10 +0000

    fdsfsdfsdf

--- Assets/myScripts/Test.cs
+++ Assets/myScripts/Test.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+  float ringu;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
